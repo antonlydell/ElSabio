8bad66d4e2e200feed065a9df072b26cb7d8ed56
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2025-11-15 20:16:42 +0100

    Added sub-package app.components
    Contains the components that make up the web app.
    
    Added the available icons:
    - ICON_ERROR
    - ICON_INFO
    - ICON_SUCCESS
    - ICON_WARNING

---
+++ src/elsabio/app/components/__init__.py
@@ -0,0 +1,18 @@
+# ElSabio
+# Copyright (C) 2025-present Anton Lydell
+# SPDX-License-Identifier: GPL-3.0-or-later
+# See the LICENSE file in the project root for details.
+
+r"""The components that make up the web app."""
+
+# Local
+from elsabio.app.components.icons import ICON_ERROR, ICON_INFO, ICON_SUCCESS, ICON_WARNING
+
+# The Public API
+__all__ = [
+    # icons
+    'ICON_ERROR',
+    'ICON_INFO',
+    'ICON_SUCCESS',
+    'ICON_WARNING',
+]
