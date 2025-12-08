920f208ebed7b6e4c8b7c87d6b29bc11ffa07de1
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2025-12-08 14:12:00 +0100

    Added DataFrame models for the Tariff Analyzer module
    A DataFrame model represents a database table, a query or some other
    table like structure. using a `pandas.DataFrame`.The DataFrame models
    of the Tariff Analyzer module will be used when importing the tariff
    related data.

--- src/elsabio/models/core.py
+++ src/elsabio/models/core.py
@@ -5,8 +5,93 @@
 
 r"""The core data models of ElSabio."""
 
+# Standard library
+from collections.abc import Mapping
+from typing import Any, ClassVar
+
 # Third party
+import pandas as pd
+from pydantic import BaseModel as PydanticBaseModel
+from pydantic import ConfigDict, ValidationError
 from streamlit_passwordless import User as User
 
-# The Public API
-__all__ = ['User']
+# Local
+from elsabio.exceptions import ElSabioError
+
+type StrMapping = Mapping[str, str]
+type ColumnList = list[str]
+
+
+class BaseModel(PydanticBaseModel):
+    r"""The BaseModel that all models inherit from."""
+
+    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True, frozen=True)
+
+    def __init__(self, **kwargs: Any) -> None:
+        try:
+            super().__init__(**kwargs)
+        except ValidationError as e:
+            raise ElSabioError(str(e)) from None
+
+
+class BaseDataFrameModel(BaseModel):
+    r"""The base model that all DataFrame models will inherit from.
+
+    A DataFrame model represents a database table, a query or some other table like structure.
+    using a :class:`pandas.DataFrame`. The DataFrame is accessible from the `df` attribute.
+    Each column name of the DataFrame should be implemented as a class variable starting with
+    the prefix "c_", e.g. `c_name: ClassVar[str] = 'name'` for a name column. The class variables
+    listed below should be defined for each subclass of this model.
+
+    Class Variables
+    ---------------
+    dtypes : ClassVar[dict[str, str]]
+        The mapping of column names to their datatypes of the DataFrame.
+
+    index_cols : ClassVar[list[str]]
+        The index columns of the DataFrame.
+
+    parse_dates : ClassVar[list[str]]
+        The columns of the DataFrame that should be parsed into datetime columns.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The contents of the model as a DataFrame.
+    """
+
+    dtypes: ClassVar[StrMapping] = {}
+    index_cols: ClassVar[ColumnList] = []
+    parse_dates: ClassVar[ColumnList] = []
+
+    df: pd.DataFrame
+
+    @property
+    def shape(self) -> tuple[int, int]:
+        r"""The shape (rows, cols) of the DataFrame."""
+
+        return self.df.shape
+
+    @property
+    def index(self) -> pd.Index:
+        r"""The index column(s) of the DataFrame."""
+
+        return self.df.index
+
+    @property
+    def row_count(self) -> int:
+        r"""The number of rows of the DataFrame."""
+
+        return self.df.shape[0]
+
+    @property
+    def empty(self) -> bool:
+        r"""Check if the DataFrame is empty or not."""
+
+        return self.row_count == 0
+
+    @property
+    def col_dtypes(self) -> pd.Series:
+        r"""The dtypes of the columns of the DataFrame."""
+
+        return self.df.dtypes
--- src/elsabio/models/tariff_analyzer.py
+++ src/elsabio/models/tariff_analyzer.py
@@ -7,6 +7,10 @@ r"""The data models of the Tariff Analyzer module."""
 
 # Standard library
 from enum import Enum
+from typing import ClassVar
+
+# Local
+from elsabio.models.core import BaseDataFrameModel, StrMapping
 
 
 class FacilityType(Enum):
@@ -99,3 +103,236 @@ class CalcStrategy(Enum):
     ACTIVE_POWER_OVERSHOOT_SUBSCRIBED_POWER = 4
     REACTIVE_POWER_CONS_OVERSHOOT_ACTIVE_POWER_CONS = 5
     REACTIVE_POWER_PROD_OVERSHOOT_ACTIVE_POWER_CONS = 6
+
+
+class FacilityMappingDataFrameModel(BaseDataFrameModel):
+    r"""A model of the facilities for mapping `ean` to `facility_id`.
+
+    Parameters
+    ----------
+    facility_id : int
+        The unique ID of the facility.
+
+    ean : int
+        The unique EAN code of the facility.
+    """
+
+    c_facility_id: ClassVar[str] = 'facility_id'
+    c_ean: ClassVar[str] = 'ean'
+
+    dtypes: ClassVar[StrMapping] = {c_facility_id: 'uint32[pyarrow]', c_ean: 'uint64[pyarrow]'}
+
+
+class FacilityDataFrameModel(BaseDataFrameModel):
+    r"""A model of the facilities represented as a DataFrame.
+
+    Parameters
+    ----------
+    facility_id : int
+        The unique ID of the facility.
+
+    ean : int
+        The unique EAN code of the facility.
+
+    ean_prod : int or None
+        The EAN code of the related production facility if the facility has one.
+
+    facility_type_id : int
+        The type of facility.
+
+    name : str or None
+        A descriptive name of the facility.
+
+    description : str or None
+        A description of the facility.
+    """
+
+    c_facility_id: ClassVar[str] = 'facility_id'
+    c_ean: ClassVar[str] = 'ean'
+    c_ean_prod: ClassVar[str] = 'ean_prod'
+    c_facility_type_id: ClassVar[str] = 'facility_type_id'
+    c_name: ClassVar[str] = 'name'
+    c_description: ClassVar[str] = 'description'
+
+    dtypes: ClassVar[StrMapping] = {
+        c_facility_id: 'uint32[pyarrow]',
+        c_ean: 'uint64[pyarrow]',
+        c_ean_prod: 'uint64[pyarrow]',
+        c_facility_type_id: 'uint8[pyarrow]',
+        c_name: 'string[pyarrow]',
+        c_description: 'string[pyarrow]',
+    }
+
+
+class ProductDataFrameModel(BaseDataFrameModel):
+    r"""Products that can be associated with facility contracts.
+
+    Parameters
+    ----------
+    product_id : int
+        The unique ID of the product.
+
+    external_id : str
+        The unique ID of the product in the parent system.
+
+    name : str
+        The unique name of the product.
+
+    description : str or None
+        A description of the product.
+    """
+
+    c_product_id: ClassVar[str] = 'product_id'
+    c_external_id: ClassVar[str] = 'external_id'
+    c_name: ClassVar[str] = 'name'
+    c_description: ClassVar[str] = 'description'
+
+    dtypes: ClassVar[StrMapping] = {
+        c_product_id: 'uint16[pyarrow]',
+        c_external_id: 'uint32[pyarrow]',
+        c_name: 'string[pyarrow]',
+        c_description: 'string[pyarrow]',
+    }
+
+
+class FacilityContractImportDataFrameModel(BaseDataFrameModel):
+    r"""Contract related information of a facility to import to the database.
+
+    Parameters
+    ----------
+    ean : int
+        The unique EAN code of the facility.
+
+    date_id : datetime.date
+        The month the contract data is valid for represented as the first day of the month in
+        in the configured business timezone of the app.
+
+    fuse_size : int or None
+        The contracted fuse size [A].
+
+    subscribed_power : float or None
+        The subscribed power [kW].
+
+    connection_power : float or None
+        The connection power of the facility [kW].
+
+    account_nr : int or None
+        The bookkeeping account of the facility contract.
+
+    customer_type_id : int
+        The ID of the type of customer associated with the facility contract.
+
+    ext_product_id : int or None
+        The external ID of the product that the facility contract belongs to. The ID
+        is external from the perspective of ElSabio and internal to the parent system.
+    """
+
+    c_ean: ClassVar[str] = 'ean'
+    c_date_id: ClassVar[str] = 'date_id'
+    c_fuse_size: ClassVar[str] = 'fuse_size'
+    c_subscribed_power: ClassVar[str] = 'subscribed_power'
+    c_connection_power: ClassVar[str] = 'connection_power'
+    c_account_nr: ClassVar[str] = 'account_nr'
+    c_customer_type_id: ClassVar[str] = 'customer_type_id'
+    c_ext_product_id: ClassVar[str] = 'ext_product_id'
+
+    dtypes: ClassVar[StrMapping] = {
+        c_ean: 'uint64[pyarrow]',
+        c_fuse_size: 'uint16[pyarrow]',
+        c_subscribed_power: 'float64[pyarrow]',
+        c_connection_power: 'float64[pyarrow]',
+        c_account_nr: 'uint16[pyarrow]',
+        c_customer_type_id: 'uint16[pyarrow]',
+        c_ext_product_id: 'uint32[pyarrow]',
+    }
+    parse_dates: ClassVar[list[str]] = [c_date_id]
+
+
+class FacilityContractDataFrameModel(BaseDataFrameModel):
+    r"""Contract related information of a facility valid per month.
+
+    Parameters
+    ----------
+    facility_id : int
+        The unique ID of the facility.
+
+    date_id : datetime.date
+        The month the contract data is valid for represented as the first day of the month in
+        in the configured business timezone of the app.
+
+    fuse_size : int or None
+        The contracted fuse size [A].
+
+    subscribed_power : float or None
+        The subscribed power [kW].
+
+    connection_power : float or None
+        The connection power of the facility [kW].
+
+    account_nr : int or None
+        The bookkeeping account of the facility contract.
+
+    customer_type_id : int
+        The ID of the type of customer associated with the facility contract.
+
+    product_id : int or None
+        The ID of the product associated with the facility contract.
+    """
+
+    c_facility_id: ClassVar[str] = 'facility_id'
+    c_date_id: ClassVar[str] = 'date_id'
+    c_fuse_size: ClassVar[str] = 'fuse_size'
+    c_subscribed_power: ClassVar[str] = 'subscribed_power'
+    c_connection_power: ClassVar[str] = 'connection_power'
+    c_account_nr: ClassVar[str] = 'account_nr'
+    c_customer_type_id: ClassVar[str] = 'customer_type_id'
+    c_product_id: ClassVar[str] = 'product_id'
+
+    dtypes: ClassVar[StrMapping] = {
+        c_facility_id: 'uint32[pyarrow]',
+        c_fuse_size: 'uint16[pyarrow]',
+        c_subscribed_power: 'float64[pyarrow]',
+        c_connection_power: 'float64[pyarrow]',
+        c_account_nr: 'uint16[pyarrow]',
+        c_customer_type_id: 'uint16[pyarrow]',
+        c_product_id: 'uint16[pyarrow]',
+    }
+    parse_dates: ClassVar[list[str]] = [c_date_id]
+
+
+class SerieValueDataFrameModel(BaseDataFrameModel):
+    r"""The values of a meter data serie.
+
+    Parameters
+    ----------
+    serie_type_id : int
+        The unique ID of the type of serie the meter data represents.
+
+    facility_id : int
+        The unique ID of the facility that the serie values belong to.
+
+    ean : int
+        The unique EAN code of the facility.
+
+    date_id : datetime.date
+        The month the meter data covers represented as the first day of the month
+        in the configured business timezone of the app.
+
+    serie_value : float
+        The meter data value.
+    """
+
+    c_serie_type_id: ClassVar[str] = 'serie_type_id'
+    c_facility_id: ClassVar[str] = 'serie_type_id'
+    c_ean: ClassVar[str] = 'ean'
+    c_date_id: ClassVar[str] = 'date_id'
+    c_serie_value: ClassVar[str] = 'serie_value'
+
+    dtypes: ClassVar[StrMapping] = {
+        c_serie_type_id: 'uint8[pyarrow]',
+        c_facility_id: 'uint32[pyarrow]',
+        c_ean: 'uint64[pyarrow]',
+        c_serie_value: 'decimal128(18,3)[pyarrow]',
+    }
+
+    parse_dates: ClassVar[list[str]] = [c_date_id]
